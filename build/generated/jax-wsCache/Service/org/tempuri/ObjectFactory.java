4c961df64581800a6e14aa26b7e6938d34a35a3d
IanPlu <personaltroll@gmail.com>
2014-04-23 13:56:54 -0700

    Re-signed and ready for the web
    Resigned both jars with the same key-- should work for web stuff now.

--- build/generated/jax-wsCache/Service/org/tempuri/ObjectFactory.java
+++ build/generated/jax-wsCache/Service/org/tempuri/ObjectFactory.java
@@ -24,10 +24,10 @@ import javax.xml.namespace.QName;
 @XmlRegistry
 public class ObjectFactory {
 
-    private final static QName _GetCodeResponseGetCodeResult_QNAME = new QName("http://tempuri.org/", "GetCodeResult");
-    private final static QName _SetCodeCode_QNAME = new QName("http://tempuri.org/", "code");
     private final static QName _SetStatusState_QNAME = new QName("http://tempuri.org/", "state");
     private final static QName _GetStatusResponseGetStatusResult_QNAME = new QName("http://tempuri.org/", "GetStatusResult");
+    private final static QName _SetCodeCode_QNAME = new QName("http://tempuri.org/", "code");
+    private final static QName _GetCodeResponseGetCodeResult_QNAME = new QName("http://tempuri.org/", "GetCodeResult");
 
     /**
      * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: org.tempuri
@@ -104,9 +104,18 @@ public class ObjectFactory {
      * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetCodeResult", scope = GetCodeResponse.class)
-    public JAXBElement<String> createGetCodeResponseGetCodeResult(String value) {
-        return new JAXBElement<String>(_GetCodeResponseGetCodeResult_QNAME, String.class, GetCodeResponse.class, value);
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "state", scope = SetStatus.class)
+    public JAXBElement<String> createSetStatusState(String value) {
+        return new JAXBElement<String>(_SetStatusState_QNAME, String.class, SetStatus.class, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetStatusResult", scope = GetStatusResponse.class)
+    public JAXBElement<String> createGetStatusResponseGetStatusResult(String value) {
+        return new JAXBElement<String>(_GetStatusResponseGetStatusResult_QNAME, String.class, GetStatusResponse.class, value);
     }
 
     /**
@@ -122,18 +131,9 @@ public class ObjectFactory {
      * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
      * 
      */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "state", scope = SetStatus.class)
-    public JAXBElement<String> createSetStatusState(String value) {
-        return new JAXBElement<String>(_SetStatusState_QNAME, String.class, SetStatus.class, value);
-    }
-
-    /**
-     * Create an instance of {@link JAXBElement }{@code <}{@link String }{@code >}}
-     * 
-     */
-    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetStatusResult", scope = GetStatusResponse.class)
-    public JAXBElement<String> createGetStatusResponseGetStatusResult(String value) {
-        return new JAXBElement<String>(_GetStatusResponseGetStatusResult_QNAME, String.class, GetStatusResponse.class, value);
+    @XmlElementDecl(namespace = "http://tempuri.org/", name = "GetCodeResult", scope = GetCodeResponse.class)
+    public JAXBElement<String> createGetCodeResponseGetCodeResult(String value) {
+        return new JAXBElement<String>(_GetCodeResponseGetCodeResult_QNAME, String.class, GetCodeResponse.class, value);
     }
 
 }
